331cb7240d381fede9769782536609ea8da6ba99
Heitor <heitor_garc@hotmail.com>
2022-12-12 14:54:05 -0300

    setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <div className="title">React App SoftLine</div>
+          <div className="subtitle">React Typescript</div>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
