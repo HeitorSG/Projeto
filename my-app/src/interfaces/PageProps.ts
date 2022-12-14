c6f965b5427c355642afb0b33296e19f13550332
Heitor <heitor_garc@hotmail.com>
2022-12-14 15:39:03 -0300

    Atualizando pagina utilizando callback

--- my-app/src/interfaces/PageProps.ts
+++ my-app/src/interfaces/PageProps.ts
@@ -24,6 +24,7 @@ export interface RecordMutationsProps<T> {
     FormFields: React.FC<FormFieldsProps<T>>;
     activeRecord: T;
     apiPath: string;
+    callback: Function;
 }
 
 export interface RecordNewProps<T> {
--- my-app/src/pages/Record/Edit.tsx
+++ my-app/src/pages/Record/Edit.tsx
@@ -10,7 +10,7 @@ export const RecordEdit = <T extends Record>({
 }: RecordEditProps<T>) => {
     return (
         <div className="edit">
-            <button className="bt-remove" onClick={() => remove(activeRecord)}></button>
+            <button className="bt-remove" onClick={() => remove(activeRecord)}>Remover</button>
             <h2>Edit</h2>
             <RecordForm 
             FormFields={FormFields} 
